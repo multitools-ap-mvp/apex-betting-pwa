463961843c42a9abd78f5e2288ddf3ca9f7b46b1
multitools-ap-mvp <apex.samsung.s9@gmail.com>
2026-05-29 04:40:56 +0200

    Add files via upload

---
+++ apps/web/src/lib/utils.ts
@@ -0,0 +1,23 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatCoins(amount: number): string {
+  return new Intl.NumberFormat('en-US').format(amount)
+}
+
+export function formatDate(date: string | Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(date))
+}
+
+export function formatOdds(odds: number): string {
+  return odds.toFixed(2) + 'x'
+}
